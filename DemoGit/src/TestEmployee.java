271a3568c2fc97db37f2f9395011b54887212133
AjithAnandhan <ajith96nov@gmail.com>
2019-05-30 16:52:02 +0530

    Merge

---
+++ DemoGit/src/TestEmployee.java
@@ -0,0 +1,14 @@
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		
+		Employee emp = new Employee();
+		emp.setEmpid(120);
+		emp.setEmpname("ajith");
+		emp.setEmpsal(1000.00);
+		
+		
+	}
+
+}
